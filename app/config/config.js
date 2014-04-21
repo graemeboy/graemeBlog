2d097962eed5a1ab5d2c0f2b66ccb8c2a7295f3d
Graeme Boy <graemeboy@gmail.com>
2014-04-21 17:48:30 -0500

    fix database

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,7 @@
 var path = require("path");
 module.exports = {
-    db: "mongodb://graemeboy:<password>hypatia",
+    //db: "mongodb://localhost/graemeboy",
+    db: "mongodb://graemeboy:hypatia@oceanic.mongohq.com:10025/app24397758"
     root: path.normalize(__dirname + "/../"),
     app: {
         name: "Graeme Boy Blog"
