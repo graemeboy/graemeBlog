a8fa3c085485ce240cac35cfc9a18cc1b7b2787a
Graeme Boy <graemeboy@gmail.com>
2014-04-25 01:47:02 -0500

    Use flat files

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,28 +2,15 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 // Index
 var index = require('../controllers/index');
 
 // Parameters
-router.param('postSlug', index.post);
+//router.param('postSlug', index.post);
 router.param('postCat', index.cat);
 
 // Home Page
 router.get('/', index.home);
-
-
-
-// Admin area
-var admin = require('../controllers/admin');
-router.get('/admin', admin.dashboard);
-router.get('/admin/login', admin.login);
-router.post('/admin/login', admin.loginProcess);
-router.get('/admin/write', admin.write);
-router.post('/admin/write', admin.writeProcess);
-router.get('/admin/write/:postSlug', admin.edit);
 
 // Specific posts (usually popular ones)
 router.get('/css-buttons', index.css12);
@@ -36,10 +23,9 @@ router.get('/valid-man', index.validMan);
 
 // Qlock
 router.get('/qlock', index.qlock);
-router.get('/php-vs-node', index.phpVsNode);
 
 // Single Posts
-//router.get('/:postSlug', index.showPost);
+router.get('/php-vs-node', index.phpVsNode);
 router.get('/cat/:postCat', index.showCatPosts);
 
 module.exports = router;
