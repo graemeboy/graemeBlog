37c57ad9528fc2600a702f6e54f6ce7c00263711
Graeme Boy <graemeboy@gmail.com>
2014-05-02 01:24:50 -0500

    Use app.listen

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 var app = express();
-var http = require('http');
+//var http = require('http');
 var config = require("./app/config/config");
 
 require("./app/config/express")(app, config);
 
 //app.set('port', 3000);
 // Server
-http.createServer(app).listen(process.env.PORT || 3000, function () {
-  console.log('Express server listening on port ' + app.get('port'));
-}); // createServer
+//http.createServer(app).listen(process.env.PORT || 3000, function () {
+//  console.log('Express server listening on port ' + app.get('port'));
+//}); // createServer
+
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
 
 // Exports
 
