ef228a0ea3daefc4fb0422c0382152b68fa4ef57
Graeme Boy <graemeboy@gmail.com>
2014-10-01 17:25:06 +0200

    Replace logout buttons post

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -378,7 +378,7 @@ exports.phpGood = function (req, res) {
 }
     
 exports.logoutButton = function (req, res) {
-    var callToAction = 'If you enjoyed this post or have something to say about it, <a href="http://twitter.com/share?text=Hey @graeme_boy, I just read your post with the CSS for a logout button">send me a tweet.</a>';
+    var callToAction = 'If you enjoyed this post or have something to say about it, <a href="http://twitter.com/share?text=Hey @graeme_boy, I just read your post with logout buttons">send me a tweet.</a>';
     
     // Render the custom page
     res.render('posts/logout-button-css', {
